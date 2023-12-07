6d4cddc28d0bb02ce1a2f62562e4832f466aec6a
william <rino881209@gmail.com>
2023-12-07 18:08:24 +0800

    init

--- app.py
+++ app.py
@@ -21,10 +21,41 @@ def callback():
         abort(400)
     return 'OK'
 
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這個看不到',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
+            actions=[
+                PostbackAction(
+                    label='偷偷傳資料',
+                    display_text='檯面上',
+                    data='action=檯面下'
+                ),
+                MessageAction(
+                    label='光明正大傳資料',
+                    text='我就是資料'
+                ),
+                URIAction(
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 import os
 if __name__ == "__main__":
