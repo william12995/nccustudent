42d7316c40dd4635bdd98b332d8fe5b79e05d39d
william <rino881209@gmail.com>
2023-12-07 20:54:35 +0800

    init

--- app.py
+++ app.py
@@ -25,47 +25,46 @@ def callback():
 
     
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = text=event.message.text
-    if re.match('測試',message):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = getURL("2").to_string()))
-        print(getURL("2").to_string())
-    else:
-        message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
-
 # @handler.add(MessageEvent, message=TextMessage)
 # def handle_message(event):
-    
 #     message = text=event.message.text
-#     if re.match('告訴我秘密',message):
-#         buttons_template_message = TemplateSendMessage(
-#         alt_text='這個看不到',
-#         template=ButtonsTemplate(
-#             thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-#             title='行銷搬進大程式',
-#             text='選單功能－TemplateSendMessage',
-#             actions=[
-#                 PostbackAction(
-#                     label='偷偷傳資料',
-#                     display_text='檯',
-#                     data='action=檯面下'
-#                 ),
-#                 MessageAction(
-#                     label='光明正大傳資料',
-#                     text= getURL(2)
-#                 ),
-#                 URIAction(
-#                     label='行銷搬進大程式',
-#                     uri='https://marketingliveincode.com/'
-#                 )
-#             ]
-#         )
-#     )
-#         line_bot_api.reply_message(event.reply_token, buttons_template_message)
+#     if re.match('測試',message):
+#         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = getURL("2").to_string()))
+#         print(getURL("2").to_string())
 #     else:
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#         message = TextSendMessage(text=event.message.text)
+#         line_bot_api.reply_message(event.reply_token, message)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這個看不到',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
+            actions=[
+                MessageAction(
+                    label='選項1',
+                    text= getURL("1").to_string()
+                ),
+                MessageAction(
+                    label='選項2',
+                    text= getURL("2").to_string()
+                ),
+                MessageAction(
+                    label='選項3',
+                    text= getURL("3").to_string()
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 import os
 if __name__ == "__main__":
